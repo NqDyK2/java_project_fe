432e6c3b9bb0b90a02e21dca77aa07040714c9e7
NqDyK2 <nguyenquyduyabc@gmail.com>
2024-05-14 19:32:26 +0700

    Third commit with 20% component header

--- src/components/client/header.tsx
+++ src/components/client/header.tsx
@@ -1,10 +1,44 @@
 import React from 'react'
+import { PiInstagramLogoFill } from "react-icons/pi";
+
 
 type Props = {}
 
 const Header = (props: Props) => {
     return (
-        <div>Header</div>
+        <>
+            <div className='bg-main-color'>
+                <div className='w-[1440px]'>
+                    <div className='grid grid-cols-2 text-white h-10'>
+                        <div className='my-auto'>
+                            <span>Kênh người bán</span> |
+                            <span> Kết nối <PiInstagramLogoFill className='inline-block text-lg mb-1 ml-2'/></span>
+                        </div>
+                        <div className='my-auto'>
+                            <p>Hello!!</p>
+                        </div>
+                    </div>
+                    <div>
+                        <div>
+
+                        </div>
+                        <div>
+
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+                <div>
+                    <div></div>
+                    <div></div>
+                </div>
+            </div>
+        </>
     )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'white': '#FFFFFF',
+        'main-color': '#68A55B',
+      }
+    },
   },
   plugins: [],
 }

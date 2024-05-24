3add6db08dd8c3e2faf22b554da348fc34e42599
NqDyK2 <nguyenquyduyabc@gmail.com>
2024-05-24 20:15:31 +0700

    Feat: Update pre release products page

--- src/features/products/Products.tsx
+++ src/features/products/Products.tsx
@@ -1,11 +1,27 @@
 import React from 'react'
+import { RxHamburgerMenu } from "react-icons/rx";
 
 type Props = {}
 
 const Products = (props: Props) => {
-  return (
-    <div>Hiii!!!!</div>
-  )
+    return (
+        <>
+            <div className='flex justify-center gap-5 mx-auto'>
+                <div >
+                    <div className='bg-white'>
+                        <div>
+                            <span className='px-2'><RxHamburgerMenu className='inline-block text-lg mb-1 mr-2' />Tất cả danh mục</span>
+                        </div>
+                    </div>
+                    <div></div>
+                    <div></div>
+                </div>
+                <div>
+                    <div>Hi chào cậu!!</div>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default Products

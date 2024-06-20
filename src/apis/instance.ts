69e6d038a8075caf0272ec207dfd1949823a78e1
DyyNq <88083327+NqDyK2@users.noreply.github.com>
2024-06-20 14:22:16 +0700

    Update instance.ts
    Signed-off-by: DyyNq <88083327+NqDyK2@users.noreply.github.com>

--- src/apis/instance.ts
+++ src/apis/instance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: "http://103.200.20.187:8080/api", 
+    baseURL: "https://api.vuonxanhconnect.online/api", 
     timeout: 1000,
     headers: { 'Content-Type': 'application/json' }
 });
